eb60622c18215e15893963c093f806fad0ed38ce
tounext <88065807+tounext@users.noreply.github.com>
2022-04-06 20:24:54 +0900

    Update retrieve.php

--- retrieve.php
+++ retrieve.php
@@ -1,1 +1,8 @@
+<php
+header("Access-Control-Allow-Credentials: true");
+
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time');
+
+php?>
